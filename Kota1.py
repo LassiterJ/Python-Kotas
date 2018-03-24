a1fbc006ae483db92f9e06ee70251a570ac8d001
Lassiter <pikapp336@me.com>
2018-03-24 18:48:45 -0400

    It works!
    wrote working code. Needs linting though

--- Kota1.py
+++ Kota1.py
@@ -1,8 +1,37 @@
-#Codingojo.org FizzBuzz
+'''Codingdojo.org FizzBuzz'''
 # Problem Description
+def has3(number):#Hello.
+  number = str(number)
+# print number
+  i = 0
+  while i < len(number):
+    variable = number[i]
+    if variable == '3':
+      return True
+    i += 1
+  return False
+def has5(number):#Hello.
+  number = str(number)
+# print number
+  i = 0
+  while i < len(number):
+    variable = number[i]
+    if variable == '5':
+      return True
+    i += 1
+  return False
+index = 0
+for i in range(1, 100):
+  if i % 3 == 0 and i % 5 == 0:
+     print "FizzBuzz"
+  elif i % 3 == 0 or has3(i):   
+     print "Fizz"
+  elif i % 5 == 0 or has5(i):
+     print "Buzz"
+  else:
+     print i
 
-Imagine the scene. You are eleven years old, and in the five minutes before the end of the lesson, your Maths teacher decides he should make his class more “fun” by introducing a “game”. He explains that he is going to point at each pupil in turn and ask them to say the next number in sequence, starting from one. The “fun” part is that if the number is divisible by three, you instead say “Fizz” and if it is divisible by five you say “Buzz”. So now your maths teacher is pointing at all of your classmates in turn, and they happily shout “one!”, “two!”, “Fizz!”, “four!”, “Buzz!”… until he very deliberately points at you, fixing you with a steely gaze… time stands still, your mouth dries up, your palms become sweatier and sweatier until you finally manage to croak “Fizz!”. Doom is avoided, and the pointing finger moves on.
 
-So of course in order to avoid embarassment infront of your whole class, you have to get the full list printed out so you know what to say. Your class has about 33 pupils and he might go round three times before the bell rings for breaktime. Next maths lesson is on Thursday. Get coding!
 
-Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz “.
+
+#
